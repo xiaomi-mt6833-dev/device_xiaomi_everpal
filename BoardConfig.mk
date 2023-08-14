963a1feaa7f822d3b4cb4ddc6f053125fb18e95e
Sushrut1101 <guptasushrut@gmail.com>
2023-08-14 23:37:20 +0530

    everpal: Configure Partitions
    Change-Id: I19109bb299a4f798b0f193bb20058ddd60cd0593
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,11 @@
 
 DEVICE_PATH := device/xiaomi/everpal
 
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
+
+BOARD_SUPER_PARTITION_SIZE := 9126805504
+BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := 9122611200
+
 # Inherit from mt6833-common
 include device/xiaomi/mt6833-common/BoardConfigCommon.mk
