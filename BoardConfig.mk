e21f57f76d013beeae2d25f179d1e19d7195b359
Sushrut1101 <guptasushrut@gmail.com>
2023-08-14 23:26:40 +0530

    everpal: Initial skeleton tree
    Change-Id: Ibfbdca05bb8a434cc07e3f4fa00f50de46c370a4
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/everpal
+
+# Inherit from mt6833-common
+include device/xiaomi/mt6833-common/BoardConfigCommon.mk
---
+++ device.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from mt6833-common
+$(call inherit-product, device/xiaomi/mt6833-common/mt6833.mk)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/xiaomi/everpal/everpal-vendor.mk)
+
+# Soong Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
