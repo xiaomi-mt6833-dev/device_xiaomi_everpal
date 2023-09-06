0628615ffd7a6b6ab475f61d8ca919ea4dacb530
Sushrut1101 <guptasushrut@gmail.com>
2023-09-06 14:20:43 +0530

    everpal: Create empty manifest file
    Change-Id: I9445bf37dbca46a0e63cb96a2366d9d0b76a81bf
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,5 +15,8 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
 BOARD_SUPER_PARTITION_SIZE := 9126805504
 BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := 9122611200
 
+# Vintf
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
+
 # Inherit from mt6833-common
 include device/xiaomi/mt6833-common/BoardConfigCommon.mk
