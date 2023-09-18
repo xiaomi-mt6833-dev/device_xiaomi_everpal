a3bd00a673881ff9f405eb51c07fed49b3475f67
Sushrut1101 <guptasushrut@gmail.com>
2023-09-18 15:40:37 +0530

    everpal: Create empty prop files
    Change-Id: I32093339beb0139bd6ebc1124ab06a69310ffe64
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,13 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
 BOARD_SUPER_PARTITION_SIZE := 9126805504
 BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := 9122611200
 
+# Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
+
 # Vintf
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 
