d5fc9eac5170af6b67a66cd8525a7186eceb07e3
Sushrut1101 <guptasushrut@gmail.com>
2023-11-26 16:52:16 +0530

    everpal: Welcome PixelOS!
    Change-Id: Iee1dab18b3ad14fa001a58948eefbd5b9d203643
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_everpal.mk
+    $(LOCAL_DIR)/aosp_everpal.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_everpal-user \
-    lineage_everpal-userdebug \
-    lineage_everpal-eng
+    aosp_everpal-user \
+    aosp_everpal-userdebug \
+    aosp_everpal-eng
--- aosp_everpal.mk
+++ aosp_everpal.mk
@@ -8,14 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from everpal device
 $(call inherit-product, device/xiaomi/everpal/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_everpal
+PRODUCT_NAME := aosp_everpal
 PRODUCT_DEVICE := everpal
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
